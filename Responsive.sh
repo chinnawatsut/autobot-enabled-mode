c975fb63cf4eb8de75fdf5a51969266ed6f4b2e2
Chinnawat K <chinnawatsut@gmail.com>
2017-06-27 17:07:21 +0700

    add responsive in Phoenix owner

---
+++ Responsive.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+echo "Autobot for Responsive"
+
+./main.py   DEV_Change_Sim_Responsive
+./main.py   DEV_ChangeProfile_Customer_Responsive
+./main.py   DEV_DeviceSale_SaleOrder_Responsive
+
+echo "Have a good day :)"
+
